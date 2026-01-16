463fdd2c3363f49bd55c5c615bff2c2ce364dd9a
Wishes38 <serkanozdemir38080@gmail.com>
2026-01-16 20:44:10 +0300

    chore: configure env vars and ci pipeline correctly for postgres

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
@@ -6,14 +7,22 @@ from fastapi.testclient import TestClient
 from app.main import app
 from app.core.database import Base, get_db
 
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///:memory:")
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
+if "sqlite" in SQLALCHEMY_DATABASE_URL:
+    connect_args = {"check_same_thread": False}
+    poolclass = StaticPool
+else:
+    connect_args = {}
+    poolclass = None
+
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, 
-    connect_args={"check_same_thread": False}, 
-    poolclass=StaticPool,
+    SQLALCHEMY_DATABASE_URL,
+    connect_args=connect_args,
+    poolclass=poolclass if poolclass else None
 )
+
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def override_get_db():
@@ -27,7 +36,6 @@ app.dependency_overrides[get_db] = override_get_db
 
 @pytest.fixture(scope="function")
 def client():
-    
     Base.metadata.create_all(bind=engine)
     
     with TestClient(app) as c:
